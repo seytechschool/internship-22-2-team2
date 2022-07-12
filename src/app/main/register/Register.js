86e95270c97bb2c288e6a7e84cdde6b0fb189bc1
unknown <aldamatov@gmail.com>
2022-07-13 08:13:10 +1000

    Login and Redister pages has been modified

--- src/app/main/register/Register.js
+++ src/app/main/register/Register.js
@@ -1,26 +1,29 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { motion } from 'framer-motion';
+import { Controller, useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+import { registerWithFirebase } from 'app/auth/store/registerSlice';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { makeStyles } from '@material-ui/core/styles';
 import { darken } from '@material-ui/core/styles/colorManipulator';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import clsx from 'clsx';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Auth0RegisterTab from './tabs/Auth0RegisterTab';
-import FirebaseRegisterTab from './tabs/FirebaseRegisterTab';
-import JWTRegisterTab from './tabs/JWTRegisterTab';
+
+import * as yup from 'yup';
+import _ from '@lodash';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
-      theme.palette.primary.dark,
-      0.5
-    )} 100%)`,
-    color: theme.palette.primary.contrastText
-  },
+  root: {},
   leftSection: {},
   rightSection: {
     background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
@@ -31,12 +34,53 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+/**
+ * Form Validation Schema
+ */
+const schema = yup.object().shape({
+  name: yup.string().required('You must enter your name'),
+  email: yup.string().email('You must enter a valid email').required('You must enter a email'),
+  password: yup
+    .string()
+    .required('Please enter your password.')
+    .min(8, 'Password is too short - should be 8 chars minimum.'),
+  passwordConfirm: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match'),
+  acceptTermsConditions: yup.boolean().oneOf([true], 'The terms and conditions must be accepted.')
+});
+
+const defaultValues = {
+  name: '',
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  acceptTermsConditions: false
+};
+
 function Register() {
   const classes = useStyles();
-  const [selectedTab, setSelectedTab] = useState(0);
+  const dispatch = useDispatch();
+  const authRegister = useSelector(({ auth }) => auth.register);
 
-  function handleTabChange(event, value) {
-    setSelectedTab(value);
+  const formRef = useRef(null);
+  const { control, formState, handleSubmit, reset, setError } = useForm({
+    mode: 'onChange',
+    defaultValues,
+    resolver: yupResolver(schema)
+  });
+
+  const { isValid, dirtyFields, errors } = formState;
+
+  useEffect(() => {
+    authRegister.errors.forEach(error => {
+      setError(error.type, {
+        type: 'manual',
+        message: error.message
+      });
+    });
+  }, [authRegister.errors, setError]);
+
+  function onSubmit(model) {
+    dispatch(registerWithFirebase(model));
   }
 
   return (
@@ -54,68 +98,143 @@ function Register() {
         >
           <CardContent className="flex flex-col items-center justify-center w-full py-96 max-w-320">
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.2 } }}>
-              <div className="flex items-center justif-center mb-32">
-                <img className="logo-icon w-48" src="assets/images/logos/fuse.svg" alt="logo" />
+              <div className="flex items-center mb-48">
+                <img
+                  style={{ background: 'black', padding: '5px' }}
+                  className="logo-icon w-48"
+                  src="https://www.emplosoft.com/assets/img/logo.png"
+                  alt="logo"
+                />
                 <div className="border-l-1 mr-4 w-1 h-40" />
                 <div>
                   <Typography className="text-24 font-semibold logo-text" color="inherit">
-                    FUSE
-                  </Typography>
-                  <Typography className="text-16 tracking-widest -mt-8 font-700" color="textSecondary">
-                    REACT
+                    EMPLOSOFT
                   </Typography>
                 </div>
               </div>
             </motion.div>
 
-            <Tabs value={selectedTab} onChange={handleTabChange} variant="fullWidth" className="w-full mb-32">
-              <Tab
-                icon={<img className="h-40 p-4 bg-black rounded-12" src="assets/images/logos/jwt.svg" alt="firebase" />}
-                className="min-w-0"
-                label="JWT"
+            <form
+              name="registerForm"
+              noValidate
+              className="flex flex-col justify-center w-full"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    className="mb-16"
+                    label="Name"
+                    autoFocus
+                    type="name"
+                    error={!!errors.name}
+                    helperText={errors?.name?.message}
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                )}
               />
-              <Tab
-                icon={<img className="h-40" src="assets/images/logos/firebase.svg" alt="firebase" />}
-                className="min-w-0"
-                label="Firebase"
-              />
-              <Tab
-                icon={<img className="h-40" src="assets/images/logos/auth0.svg" alt="auth0" />}
-                className="min-w-0"
-                label="Auth0"
-              />
-            </Tabs>
 
-            {selectedTab === 0 && <JWTRegisterTab />}
-            {selectedTab === 1 && <FirebaseRegisterTab />}
-            {selectedTab === 2 && <Auth0RegisterTab />}
-          </CardContent>
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    className="mb-16"
+                    label="Email"
+                    type="email"
+                    error={!!errors.email}
+                    helperText={errors?.email?.message}
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                )}
+              />
 
-          <div className="flex flex-col items-center justify-center pb-32">
-            <div>
-              <span className="font-normal mr-8">Already have an account?</span>
-              <Link className="font-normal" to="/login">
+              <Controller
+                name="password"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    className="mb-16"
+                    label="Password"
+                    type="password"
+                    error={!!errors.password}
+                    helperText={errors?.password?.message}
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                )}
+              />
+
+              <Controller
+                name="passwordConfirm"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    className="mb-16"
+                    label="Password (Confirm)"
+                    type="password"
+                    error={!!errors.passwordConfirm}
+                    helperText={errors?.passwordConfirm?.message}
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                )}
+              />
+
+              <Controller
+                name="acceptTermsConditions"
+                control={control}
+                render={({ field }) => (
+                  <FormControl className="items-center" error={!!errors.acceptTermsConditions}>
+                    <FormControlLabel label="Term and Conditions" control={<Checkbox {...field} />} />
+                    <FormHelperText>{errors?.acceptTermsConditions?.message}</FormHelperText>
+                  </FormControl>
+                )}
+              />
+
+              <Button
+                variant="contained"
+                color="primary"
+                className="w-full mx-auto mt-16"
+                aria-label="Register"
+                disabled={_.isEmpty(dirtyFields) || !isValid}
+                type="submit"
+              >
+                Create an account
+              </Button>
+            </form>
+            <div className="flex flex-col items-center justify-center pb-32 mt-4" style={{ marginTop: '30px' }}>
+              <span className="font-normal">Already have an account?</span>
+              <Link className="font-normal" style={{ color: '#008900' }} to="/pages/auth/login-3">
                 Login
               </Link>
             </div>
-            <Link className="font-normal mt-8" to="/">
-              Back to Dashboard
-            </Link>
-          </div>
+          </CardContent>
         </Card>
 
         <div className={clsx(classes.rightSection, 'hidden md:flex flex-1 items-center justify-center p-64')}>
           <div className="max-w-320">
             <motion.div initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}>
-              <Typography variant="h3" color="inherit" className="font-semibold leading-tight">
-                Welcome <br />
-                to the <br /> FUSE React!
+              <Typography color="inherit" className="text-32 sm:text-44 font-semibold leading-tight">
+                EmploSoft <br />
               </Typography>
             </motion.div>
 
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.3 } }}>
-              <Typography variant="subtitle1" color="inherit" className="mt-32">
-                Powerful and professional admin template for Web Applications, CRM, CMS, Admin Panels and more.
+              <Typography variant="subtitle1" color="inherit" className="mt-32 font-medium">
+                Slogan
               </Typography>
             </motion.div>
           </div>
