66270290444f8e41f9aee0d14a344c15cf0b85a7
Gyzylgul <gyzyl89@gmail.com>
2022-07-22 13:03:26 -0700

    ForgotPasswordPageMain-error msg

--- src/app/auth/store/loginSlice.js
+++ src/app/auth/store/loginSlice.js
@@ -1,5 +1,3 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
 import { createSlice } from '@reduxjs/toolkit';
 import { showMessage } from 'app/store/fuse/messageSlice';
 import firebaseService from 'app/services/firebaseService';
@@ -30,8 +28,7 @@ export const submitLoginWithFireBase =
 
       return () => false;
     }
-    console.log(email, 'email');
-    console.log(password, 'password');
+
     return firebaseService.auth
       .signInWithEmailAndPassword(email, password)
       .then(() => {
@@ -78,17 +75,13 @@ export const forgotPasswordFirebase =
 
       return () => false;
     }
-    console.log(email, 'FSemail');
-    console.log(firebaseService.auth, 'firebaseService.auth');
 
     return firebaseService.auth
       .sendPasswordResetEmail(email)
       .then(() => {
         // alert('Please check your email...');
-        // console.log('hello');
       })
       .catch(error => {
-        console.log(error, 'error');
         const emailErrorCodes = [
           'auth/email-already-in-use',
           'auth/invalid-email',
@@ -118,58 +111,12 @@ export const forgotPasswordFirebase =
         }
 
         if (error.code === 'auth/user-not-found') {
-          // alert('user not found');
-          // dispatch(showMessage({ message: error.message }));
+          dispatch(showMessage({ message: error.message }));
         }
 
         return dispatch(loginError(response));
       });
   };
-// export const ResetPassword =
-//   ({ email, password }) =>
-//   async dispatch => {
-//     if (!firebaseService.auth) {
-//       console.warn("Firebase Service didn't initialize, check your configuration");
-
-//       return () => false;
-//     }
-//     return firebaseService.auth
-//       .signInWithEmailAndPassword(email, password)
-//       .then(() => {
-//         return dispatch(loginSuccess());
-//       })
-//       .catch(error => {
-//         const emailErrorCodes = [
-//           'auth/email-already-in-use',
-//           'auth/invalid-email',
-//           'auth/operation-not-allowed',
-//           'auth/user-not-found',
-//           'auth/user-disabled'
-//         ];
-//         const passwordErrorCodes = ['auth/weak-password', 'auth/wrong-password'];
-//         const response = [];
-
-//         if (emailErrorCodes.includes(error.code)) {
-//           response.push({
-//             type: 'email',
-//             message: error.message
-//           });
-//         }
-
-//         if (passwordErrorCodes.includes(error.code)) {
-//           response.push({
-//             type: 'password',
-//             message: error.message
-//           });
-//         }
-
-//         if (error.code === 'auth/invalid-api-key') {
-//           dispatch(showMessage({ message: error.message }));
-//         }
-
-//         return dispatch(loginError(response));
-//       });
-//   };
 
 const initialState = {
   success: false,
